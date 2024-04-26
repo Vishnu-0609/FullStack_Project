cd49baf2af105af9a5686c7abbb7bf1ebd3d1ab7
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-04-26 09:33:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,13 +26,6 @@ connectDB()
 
 const app=express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin if needed
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(cors({
     origin:"https://full-stack-project-bmv1.vercel.app",
     methods:["GET","POST"],
