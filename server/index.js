77a20c9fc94d58a9ffba20c07f8f1581be242ac2
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-04-26 11:06:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ const server = createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"https://full-stack-project-three.vercel.app/socket.io/?EIO=4&transport=polling&t=OyOutCe",
+        origin:"https://full-stack-project-three.vercel.app/",
         methods:["GET","POST"],
         credentials:true
     }
