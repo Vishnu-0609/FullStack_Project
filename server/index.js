67a3726f6fdf0e04d8c4ceea8e17b74f16fb6610
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-06-08 17:18:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,63 +16,43 @@ dotenv.config({
     path:"./.env"
 })
 
-connectDB()
-.then(() => {
-    
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
-
 const app=express();
+
+app.use(cors({
+    origin:process.env.FRONTENED_HOST,
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}));
 
 const server = createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"https://full-stack-project-bmv1.vercel.app",
-        methods:["GET","POST"],
+        origin:process.env.FRONTENED_HOST,
+        methods:["GET","POST","PUT","DELETE"],
         credentials:true
     }
-}); 
-
-// https://full-stack-project-bmv1.vercel.app
-const corsOptions = {
-    origin: "https://full-stack-project-bmv1.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true
-};
-
-
-io.use((socket, next) => {
-    cors(corsOptions)(socket.request, {}, next);
-});
-app.use(cors(corsOptions)); 
-
-// const io = new Server(server);
-
-// app.use(cors({
-//     origin:"https://full-stack-project-bmv1.vercel.app",
-//     methods:["GET","POST"],
-//     credentials:true
-// }));
+});  
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
 import { router } from "./src/routes/route.js";
 
 app.use("/api/v1/user",router);
 
-// circuit
 
+// circuit
 
 const defaultValue = "";
 
 io.on("connection",(socket)=>{
-    // console.log("User Connected ",socket.id);
 
     socket.on("get-document",async({documentId,documentName})=>{
     
@@ -126,24 +106,28 @@ io.on("connection",(socket)=>{
     // socket.broadcast.emit("welcome",`${socket.id} joined server`);
 })
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+connectDB()
+.then(()=>{
+    server.listen(process.env.PORT,()=>
+    {
+        console.log(`Server is running on ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log(err);
 })
 
-server.listen(process.env.PORT,()=>
+const findOrCreateDocument = async (id) =>
 {
-    console.log(`Server is running on ${process.env.PORT}`);
-});
-
-const findOrCreateDocument = async (id) => {
-    if (!id) {
-        throw new Error("Document ID is null");
-    }
+    if(id==null) return;
     const document = await Documentmodel.findById(id);
-    if (document) {
-        return document;
-    } else {
-        return await Documentmodel.create({ _id: id, data: defaultValue });
-    }
-};
 
+    if(document)
+    {
+        return document;
+    }
+    else
+    {
+        return await Documentmodel.create({_id:id,data:defaultValue});
+    }
+}
